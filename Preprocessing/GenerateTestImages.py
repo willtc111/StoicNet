2a4ded7b696dc521d8d083211d3794028351a04c
William <willtc111@gmail.com>
2020-04-23 17:02:25 -0400

    Added better support for novel images

--- Preprocessing/GenerateTestImages.py
+++ Preprocessing/GenerateTestImages.py
@@ -15,34 +15,38 @@ faceCascadeTree = cv2.CascadeClassifier(r'C:\OpenCV-3.4.5\opencv\build\etc\haarc
 def faceCrop(image):
     imageCV = pil2cv(image)
 
-    min_size = (150,150)
-    haar_scale = 1.1
-    min_neighbors = 3
-    haar_flags = 0
-    border_size = 50
+    border_size = 25
+    sf = 1.1
+    mn = 3
+    ms = (90,90)
 
     # Detect the faces
     imageCV2 = cv2.equalizeHist(imageCV)
-    faces = faceCascade.detectMultiScale(imageCV2, haar_scale, min_neighbors, haar_flags, min_size)
+    faces = faceCascade.detectMultiScale(imageCV2, scaleFactor=sf, minNeighbors=mn, minSize=ms)
 
-    
     # Handling failure (mostly false positives)
-    if (not faces.any()) or faces.shape[0] != 1:
-        facesAlt = faceCascadeAlt.detectMultiScale(imageCV2, haar_scale, min_neighbors, haar_flags, min_size)
+    if len(faces) != 1 or faces.shape[0] != 1:
+        facesAlt = faceCascadeAlt.detectMultiScale(imageCV2, scaleFactor=sf, minNeighbors=mn, minSize=ms)
         # Handle more failure
-        if (not facesAlt.any()) or facesAlt.shape[0] != 1:
-            facesTree = faceCascadeTree.detectMultiScale(imageCV2, haar_scale, min_neighbors, haar_flags, min_size)
+        if len(facesAlt) != 1 or facesAlt.shape[0] != 1:
+            facesTree = faceCascadeTree.detectMultiScale(imageCV2, scaleFactor=sf, minNeighbors=mn, minSize=ms)
             # Handle even more failure
-            if (not facesTree.any()) or facesTree.shape[0] != 1:
-                faces = faceCascade.detectMultiScale(imageCV2)
-                if not faces.any():
+            if len(facesTree) != 1 or facesTree.shape[0] != 1:
+                faces = faceCascadeTree.detectMultiScale(imageCV2, minSize=ms)
+                if len(faces) < 1 or faces.shape[0] < 1:
                     raise Exception("Could not properly detect face for image")
                 else:
-                    faces = faces[0]
+                    # Take the largest face
+                    bestFace = [0,0,0,0]
+                    for face in faces:
+                        if face[3] > bestFace[3]:
+                            bestFace = face
+                    faces = [bestFace]
             else:
                 faces = facesTree
         else:
             faces = facesAlt
+
 
     # TODO: Comment this out for final run
     for ((x, y, w, h)) in faces:
@@ -80,64 +84,50 @@ def cv2pil(cv_im):
 def fixImage(fromPath, toPath):
     img = Image.open(fromPath)
     
-    cv2.imshow('Resized', pil2cv(img))
-    cv2.waitKey(0)
+    # cv2.imshow('Resized', pil2cv(img))
+    # cv2.waitKey(0)
     
     # Crop
     img = faceCrop(img)
     
-    cv2.imshow('Resized', pil2cv(img))
-    cv2.waitKey(0)
+    # cv2.imshow('Resized', pil2cv(img))
+    # cv2.waitKey(0)
     
     # Resize
     img.thumbnail((256,256), Image.ANTIALIAS)
     
-    cv2.imshow('Resized', pil2cv(img))
-    cv2.waitKey(0)
+    # cv2.imshow('Resized', pil2cv(img))
+    # cv2.waitKey(0)
 
     img.save(toPath)
-
-def getRandomImg(subjectIndex):
-    ri = random.randint(0,subjectCount-2) # -1 for proper indexing, -1 to skip current index
-    if ri >= subIndex:
-        ri += 1 # skip over the current index
-    subjectEntry = subjectEntrys[ri].name
-
-    with os.scandir(dbRoot+ "\\" + subjectEntry) as sessionEntries:
-        sessionEntrys = list(sessionEntries)
-        ri = random.randint(0,len(sessionEntrys)-1)
-        sessionEntry = sessionEntrys[ri]
-        if sessionEntry.is_dir():
-            imageEntries = os.listdir(dbRoot+"\\"+ subjectEntry +"\\"+  sessionEntry.name)
-            ri = random.randint(0,len(imageEntries)-1)
-            imageEntry = imageEntries[ri]
-            if imageEntry.endswith('.png'):
-                return subjectEntry +"\\"+ sessionEntry.name + "\\" + imageEntry
-            else:
-                print("D'OH!:" +dbRoot+"\\"+subjectEntry+"\\"+sessionEntry.name+"\\"+imageEntry)
-                input("PUSH ENTER TO EXTERMINATE IMPURITY AND CONTINUE")
-                os.remove(dbRoot+"\\"+subjectEntry+"\\"+sessionEntry.name+"\\"+imageEntry)
-                return getRandomImg(subjectIndex)
-        else:
-            print("D'OH!:" +dbRoot+"\\"+subjectEntry+"\\"+sessionEntry.name)
-            input("PUSH ENTER TO EXTERMINATE IMPURITY AND CONTINUE")
-            os.remove(dbRoot+"\\"+subjectEntry+"\\"+sessionEntry.name)
-            return getRandomImg(subjectIndex)
-            
 
 dbRoot = r'C:\Users\Will\Documents\StoicNetData\Custom\inputs'
 destRoot = r'C:\Users\Will\Documents\StoicNetData\Custom'
 
+imageList = []
+
 imageEntries = os.listdir(dbRoot)
-counter = 1
+newCount = 0
+failCount = 0
 for imageEntry in imageEntries:
     if imageEntry.endswith('.png') or imageEntry.endswith('.jpg'):
-        print("|--" + imageEntry)
-        
-        fixImage(
-            dbRoot +"\\"+ imageEntry,
-            destRoot + "\\test{0}.png".format(counter)
-        )
-        counter += 1
+        print("   |--" + imageEntry, end='\t')
+        try:
+            fixImage(
+                dbRoot +"\\"+ imageEntry,
+                destRoot + "\\" + imageEntry
+            )
+        except Exception as ex:
+            print(" -\t{0}".format(ex))
+            failCount += 1
+        else:
+            print(" + ")
+            imageList.append(imageEntry)
+            newCount += 1
 
+print(str(newCount) + ' new images')
+print(str(failCount) + ' failures')
 
+# Write pair list to file
+with open(destRoot + '\\images.txt', 'w') as fp:
+    fp.write('\n'.join('{0}'.format(ip) for ip in imageList))
