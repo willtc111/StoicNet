fdd7dea434bf06d720397ea6bd11bae07f549b37
William <willtc111@gmail.com>
2020-04-26 19:59:48 -0400

    More neutralization analysis

--- Analysis/NeutralizationAnalysis.py
+++ Analysis/NeutralizationAnalysis.py
@@ -14,33 +14,46 @@ print('{} samples'.format(numSamples))
 ancTotal = 0.0
 genTotal = 0.0
 posTotal = 0.0
+ancMax = 0.0
+genMax = 0.0
+posMax = 0.0
 for i in range(0, numSamples):
     sample = os.path.basename(os.path.normpath(samples[i]))
+    print("Analyzing {}.".format(sample))
     ancFile = '{}anc.csv'.format(samples[i])
     genFile = '{}gen.csv'.format(samples[i])
     posFile = '{}pos.csv'.format(samples[i])
 
-    with open('ancFile', newline='') as csvfile:
+    with open(ancFile) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         data = list(reader)
-        aus = data[1][2:18]
-        print(aus)
+        aus = [float(s) for s in data[1][2:18]]
+        #print(aus)
         ancTotal += sum(aus)
+        ancMax += max(aus)
         
-    with open('genFile', newline='') as csvfile:
+    with open(genFile) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         data = list(reader)
-        aus = data[1][2:18]
-        print(aus)
+        aus = [float(s) for s in data[1][2:18]]
+        #print(aus)
         genTotal += sum(aus)
+        genMax += max(aus)
         
-    with open('posFile', newline='') as csvfile:
+    with open(posFile) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         data = list(reader)
-        aus = data[1][2:18]
-        print(aus)
+        aus = [float(s) for s in data[1][2:18]]
+        #print(aus)
         posTotal += sum(aus)
+        posMax += max(aus)
     
-    print("Analyzed {}.".format(sample))
-
+print("Average Totals:")
+print(ancTotal/float(numSamples))
+print(genTotal/float(numSamples))
+print(posTotal/float(numSamples))
+print("Average Max:")
+print(ancMax/float(numSamples))
+print(genMax/float(numSamples))
+print(posMax/float(numSamples))
 print("Done")
